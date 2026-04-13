577463c95949fb927f401e33d8f2d558f5543846
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-04-13 02:48:40 +0200

    Create index.js

---
+++ skills/general/index.js
@@ -0,0 +1,10 @@
+// skills/general/index.js
+module.exports = {
+  name: 'general',
+  tools: [
+    { name: 'calculate', description: 'Math calculations' /* ... add execute fn */ },
+    { name: 'time', description: 'Current time and date' },
+    { name: 'system_info', description: 'Host system information' },
+    { name: 'random', description: 'Generate UUID or password' },
+  ]
+};
