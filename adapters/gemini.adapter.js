48317b247139526c79226bd9ba68ed27295d2b54
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-04-14 14:11:06 +0200

    Update gemini.adapter.js

--- adapters/gemini.adapter.js
+++ adapters/gemini.adapter.js
@@ -1,3 +1,29 @@
+'use strict';
+
+const axios           = require('axios');
+const { BaseAdapter } = require('./base.adapter');
+
+class GeminiAdapter extends BaseAdapter {
+    constructor(apiKey) {
+        super('gemini');
+        this.apiKey = apiKey;
+    }
+
+    async generate(prompt, options = {}) {
+        const model = options.model || 'gemini-1.5-pro';
+        const res = await axios.post(
+            `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${this.apiKey}`,
+            { contents: [{ parts: [{ text: prompt }] }] }
+        );
+        return {
+            text:     res.data.candidates?.[0]?.content?.parts?.[0]?.text || '',
+            provider: 'gemini'
+        };
+    }
+}
+
+module.exports = { GeminiAdapter };
+
 import { BaseAdapter } from "./base.adapter.js";
 import axios from "axios";
 
