1fce71c50e8a393192d4c5bd48c2ca275903f5e8
Brighton Mzacana <markertwithbrighton@gmail.com>
2026-04-18 17:01:33 +0200

    Update index.js

--- src/payments/index.js
+++ src/payments/index.js
@@ -5,12 +5,23 @@ const { PaymentGateway } = require('./payment-gateway');
 const PaymentService = require('./payment-service');
 const webhookHandler = require('./webhook-handler');
 
+const PesaPalIntegration = require('./pesapal-integration');
+const PesaPalProvider = require('./providers/pesapal-provider');
+const setupPesaPalRoutes = require('./routes/pesapal-webhooks');
+const setupPesaPalCommands = require('./commands/pesapal-commands');
+
+
 module.exports = {
   PaymentGateway,
   PaymentService,
   webhookHandler,
+   PesaPalIntegration,
+  PesaPalProvider,
+  setupPesaPalRoutes,
+  setupPesaPalCommands,
   
   // Factory function for easy initialization
+  
   createPaymentService: (config) => {
     const gateway = new PaymentGateway(config);
     return new PaymentService(gateway);
